4c042478a4fddd206b6c8daf05e300644599d505
tatsuya828828 <kkjy011894@gmail.com>
2020-10-19 23:31:36 +0900

    planに関するid名を変更

--- app/src/main/java/com/kuma/todolist/ui/plan/PlanFragment.java
+++ app/src/main/java/com/kuma/todolist/ui/plan/PlanFragment.java
@@ -1,4 +1,4 @@
-package com.kuma.todolist.ui.dashboard;
+package com.kuma.todolist.ui.plan;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.kuma.todolist.R;
 
-public class DashboardFragment extends Fragment {
+public class PlanFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private PlanViewModel planViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        planViewModel =
+                new ViewModelProvider(this).get(PlanViewModel.class);
         View root = inflater.inflate(R.layout.fragment_plan, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final TextView textView = root.findViewById(R.id.text_plan);
+        planViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/kuma/todolist/ui/plan/PlanViewModel.java
+++ app/src/main/java/com/kuma/todolist/ui/plan/PlanViewModel.java
@@ -1,14 +1,14 @@
-package com.kuma.todolist.ui.dashboard;
+package com.kuma.todolist.ui.plan;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class PlanViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public PlanViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
