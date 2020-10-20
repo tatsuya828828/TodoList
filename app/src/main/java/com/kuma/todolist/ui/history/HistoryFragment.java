04afaa813e8b1c2f3cb5dc0a907c070647c884d5
tatsuya828828 <kkjy011894@gmail.com>
2020-10-20 09:58:34 +0900

    historyに関するid名の変更

--- app/src/main/java/com/kuma/todolist/ui/history/HistoryFragment.java
+++ app/src/main/java/com/kuma/todolist/ui/history/HistoryFragment.java
@@ -1,4 +1,4 @@
-package com.kuma.todolist.ui.notifications;
+package com.kuma.todolist.ui.history;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.kuma.todolist.R;
 
-public class NotificationsFragment extends Fragment {
+public class HistoryFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private HistoryViewModel historyViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        historyViewModel =
+                new ViewModelProvider(this).get(HistoryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_history, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final TextView textView = root.findViewById(R.id.text_history);
+        historyViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/kuma/todolist/ui/history/HistoryViewModel.java
+++ app/src/main/java/com/kuma/todolist/ui/history/HistoryViewModel.java
@@ -1,14 +1,14 @@
-package com.kuma.todolist.ui.notifications;
+package com.kuma.todolist.ui.history;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class HistoryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public HistoryViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is notifications fragment");
     }
