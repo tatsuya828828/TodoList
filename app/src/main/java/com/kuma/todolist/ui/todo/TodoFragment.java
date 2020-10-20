0d8f774abde337a01f0c5aa13782a9ba1aa82461
tatsuya828828 <kkjy011894@gmail.com>
2020-10-20 11:46:53 +0900

    入力フォーム修正

--- app/src/main/java/com/kuma/todolist/ui/todo/TodoFragment.java
+++ app/src/main/java/com/kuma/todolist/ui/todo/TodoFragment.java
@@ -1,9 +1,12 @@
 package com.kuma.todolist.ui.todo;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,11 +26,16 @@ public class TodoFragment extends Fragment {
         todoViewModel =
                 new ViewModelProvider(this).get(TodoViewModel.class);
         View root = inflater.inflate(R.layout.fragment_todo, container, false);
-        final TextView textView = root.findViewById(R.id.text_todo);
-        todoViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        Button button = root.findViewById(R.id.todo_button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Log.d("TodoFragment", "Clicked");
+                EditText editText = root.findViewById(R.id.editText);
+                TextView textView = root.findViewById(R.id.todoText);
+                String text = editText.getText().toString();
+                textView.setText(text);
+                Log.d("TodoFragment", text);
             }
         });
         return root;
